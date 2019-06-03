192e5e05f8cb9110d860af63a7fb7bdc5e35cf11
M <wu-liang@WMacBook-Pro.local>
2019-06-03 19:39:48 +0800

    Site updated: 2019-06-03 19:39:48

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2019":{"5":{"1":true}}}';
+      var data = '{"2019":{"5":{"1":true,"4":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
